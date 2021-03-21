8b2c464054375abef74430e474ae142ea93cc601
clitourist <1300135+clitourist@users.noreply.github.com>
2021-03-21 12:36:44 +0530

    trial 1

--- src/overload.java
+++ src/overload.java
@@ -1,30 +1,30 @@
 class OverloadDemo{
         void test(){
-        System.out.println("Noparameters");
+        System.out.println("No parameters");
         }
-//Overloadtestforoneintegerparameter.
+//Overload test for one integer parameter.
         void test(int a){
         System.out.println("a:"+a);
         }
-//Overloadtestfortwointegerparameters.
+//Overload test for two integer parameters.
         void test(int a,int b){
         System.out.println("a and b:"+a+""+b);
         }
-//overloadtestforadoubleparameter
+//overload test for a double parameter
         double test(double a){
         System.out.println("double a:"+a);
         return a*a;
         }
         }
         class Overload{
-        public static void main(String args[]){
+        public static void main(String[] args){
         OverloadDemo ob=new OverloadDemo();
         double result;
-//callallversionsoftest()
+//call all versions of test()
         ob.test();
         ob.test(10);
         ob.test(10,20);
         result=ob.test(123.25);
-        System.out.println("Resultofob.test(123.25):"+result);
+        System.out.println("Result of ob.test(123.25):"+result);
         }
         }
